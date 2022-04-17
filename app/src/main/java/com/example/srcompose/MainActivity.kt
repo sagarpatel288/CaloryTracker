7f4f79aafd27423cdd59d847904a75c6908704ca
sagarpatel288 <sagarpatel288@gmail.com>
2022-04-17 14:46:46 +0530

    commit#7
    7.1. Type: Examples
    7.1.1. Add examples of composable in MainActivity.kt

--- app/src/main/java/com/example/srcompose/MainActivity.kt
+++ app/src/main/java/com/example/srcompose/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import com.example.srcompose.ui.theme.SrComposeTheme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,10 +54,32 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    RootLayout {
-        Text(text = "TextView Test")
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Magenta),
+    ) {
+        Text(
+            text = "TextView 1",
+            modifier = Modifier
+                .padding(16.dp)
+                .wrapContentWidth()
+                .background(Color.Gray),
+            fontSize = 18.sp,
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = "TextView 2",
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.Cyan)
+        )
     }
 }
+
+
+
 
 
 
--- onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -1,10 +1,21 @@
 package com.example.onboarding_presentation.welcome
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun WelcomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
+    }
 }
 
 
